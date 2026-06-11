79e4d359982053db003e8c3be398f68eb94c8449
Umeyrp <umeyr.kemiksiz@gmail.com>
2026-06-11 14:13:26 +0200

    feature: enhance search functionality and loading feedback

--- script.js
+++ script.js
@@ -4,22 +4,20 @@ let pokeIDCounter_start = 1;
 let pokeIDCounter_end = 20;
 
 const dialogRef = document.getElementById('pokeinfo');
+const contentRef = document.getElementById('content');
 
 async function init() {
     if (!getPokemonFromLocalStorage()) await fetchPokemons(), savePokemonInLocalStorage();;
     renderPokemon(allPokemon);
-    console.log(allPokemon);
 }
 
 async function fetchPokemons() {
-    showLoadingFeedback();
-    hideLoadMoreButton();
-    let fetchedPokemon = [];
+    startLoading();
     const promises = [];
     try {
-        for (pokeIDCounter_start; pokeIDCounter_start <= pokeIDCounter_end; pokeIDCounter_start++) {
+        for (let id = pokeIDCounter_start; id <= pokeIDCounter_end; id++) {
             promises.push(
-                fetch(`https://pokeapi.co/api/v2/pokemon/${pokeIDCounter_start}/`)
+                fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
                     .then(response => {
                         if (!response.ok) {
                             throw new Error(`HTTP-Error: ${response.status}`);
@@ -28,14 +26,13 @@ async function fetchPokemons() {
                     })
             );
         }
-        let fetchedPokemon = await Promise.all(promises);
+        const fetchedPokemon = await Promise.all(promises);
         allPokemon.push(...fetchedPokemon);
-        hideLoadingFeedback();
-        showLoadMoreButton();
         lastFetchedPokemon = fetchedPokemon;
     } catch (error) {
         console.error('Loading error:', error);
     }
+    finishLoading();
 }
 
 function savePokemonInLocalStorage() {
@@ -46,29 +43,38 @@ function savePokemonInLocalStorage() {
 
 function getPokemonFromLocalStorage() {
     const localPokemon = JSON.parse(localStorage.getItem("pokemon"));
-    if (localPokemon) {
+    if (localPokemon && localPokemon.length > 0) {
         allPokemon = localPokemon;
-        pokeIDCounter_start = 21;
         return true;
     }
 }
 
+function finishLoading() {
+    hideLoadingFeedback();
+    showLoadMoreButton();
+}
+
+function startLoading() {
+    showLoadingFeedback();
+    hideLoadMoreButton();
+}
+
 async function fetchMorePokemons() {
-    pokeIDCounter_end += 40;
+    pokeIDCounter_start += 20;
+    pokeIDCounter_end += 20;
     await fetchPokemons();
     appendFetchedPokemon(lastFetchedPokemon);
 }
 
 function appendFetchedPokemon(pokemonArray) {
-    document.getElementById('content').innerHTML += getPokemonTemplate(pokemonArray);
+    contentRef.innerHTML += getPokemonTemplate(pokemonArray);
     showLoadMoreButton();
 }
 
 function searchPokemon() {
     const search_input = document.getElementById('search_pokemon_input').value.toLowerCase();
     if (search_input.length < 3 || search_input == "") return renderPokemon(allPokemon), showLoadMoreButton();
-    showLoadingFeedback();
-    hideLoadMoreButton();
+    startLoading();
     const searchedPokemon = allPokemon.filter(pokemon => pokemon.name.includes(search_input));
     if (searchedPokemon.length === 0) {
         showNoFoundMessage();
@@ -80,37 +86,52 @@ function searchPokemon() {
 }
 
 function showNoFoundMessage() {
-    document.getElementById('content').innerHTML = "<p>Keine Pokemon gefunden!</p>"
+    contentRef.innerHTML = `<div class="empty-state">
+                                <img src="./assets/icon/pokeball_icon.png" height="80">
+                                <h2>Kein Pokémon gefunden</h2>
+                                <p>Versuch einen anderen Namen oder Tippfehler zu vermeiden.</p>
+                            </div>`;
 }
 
 function renderPokemon(pokemonArray) {
-    document.getElementById('content').innerHTML = getPokemonTemplate(pokemonArray);
-    hideLoadingFeedback();
-    showLoadMoreButton();
+    contentRef.innerHTML = getPokemonTemplate(pokemonArray);
+    finishLoading();
 }
-
 function getPokemonTemplate(pokemonArray) {
     let html = "";
     pokemonArray.forEach(pokemon => {
-        html += `<article onclick="showPokeDetails(${pokemon.id})" class="pokemon_card" style="background-color: ${colours[pokemon.types[0].type.name]};">`;
-        html += pokemon.id + pokemon.name.toUpperCase() + `<img height="50px" src="${pokemon.sprites.other["official-artwork"].front_default}">`;
-        pokemon.types.forEach(element => {
-            html += element.type.name + " ";
-        });
-        html += `</article>`;
-        html += "<br>";
+        let typesText = "";
+        for (let i = 0; i < pokemon.types.length; i++) {
+            typesText += pokemon.types[i].type.name;
+
+            if (i < pokemon.types.length - 1) {
+                typesText += " / ";
+            }
+        }
+        html += `<article onclick="showPokeDetails(${pokemon.id})"
+                        class="pokemon_card"
+                        style="background-color: ${colours[pokemon.types[0].type.name]};">
+
+                    <div class="name">#${pokemon.id} ${pokemon.name}</div>
+
+                    <img src="${pokemon.sprites.other["official-artwork"].front_default}" height="80">
+
+                    <div class="type">
+                        ${typesText}
+                    </div>
+                </article>`;
     });
     return html;
 }
 
 function hideLoadMoreButton() {
     document.getElementById('load_more').disabled = true;
-    document.getElementById('load_more').hidden = true;
+    document.getElementById('load_more').style.display = "none";
 }
 
 function showLoadMoreButton() {
     document.getElementById('load_more').disabled = false;
-    document.getElementById('load_more').hidden = false;
+    document.getElementById('load_more').style.display = "block";
 }
 
 function showLoadingFeedback() {
@@ -129,34 +150,70 @@ async function showPokeDetails(pokeId) {
     const pokemon = getPokemonById(pokeId);
     const evo = await fetchEvoChain(pokemon);
     const stats = getPokeStats(pokemon);
-    let html = "";
-
-
-    html += `<article onclick="showPokeDetails(${pokemon.id})" class="pokemon_card" style="background-color: ${colours[pokemon.types[0].type.name]};">`;
-    html += pokemon.id + pokemon.name.toUpperCase() + `<img height="50px" src="${pokemon.sprites.other["official-artwork"].front_default}">`;
-    pokemon.types.forEach(element => {
-        html += element.type.name + " ";
-    });
-    stats.forEach(stat => {
-        html += `<p>${stat}</p>`;
-    });
-    console.log(evo);
-    html += getEvoTemplate(evo);
-    html += `</article>`;
+    let typesText = "";
+    for (let i = 0; i < pokemon.types.length; i++) {
+        typesText += pokemon.types[i].type.name;
+        if (i < pokemon.types.length - 1) {
+            typesText += " / ";
+        }
+    }
+    let statsHtml = "";
+    for (let i = 0; i < stats.length; i++) {
+        statsHtml += `<div>${stats[i]}</div>`;
+    }
+    let html = `<div class="detail-header"
+                    style="background:${colours[pokemon.types[0].type.name]}">
+                    <h2>#${pokemon.id} ${pokemon.name.toUpperCase()}</h2>
+                    <img src="${pokemon.sprites.other["official-artwork"].front_default}" width="160">
+                </div>
+                <div class="dialog-content">
+                    <div class="section-title">Types</div>
+                    <p>${typesText}</p>
+                    <div class="section-title">Stats</div>
+                    <div class="stats">
+                        ${statsHtml}
+                    </div>
+                    <div class="section-title">Evolution</div>
+                    <div class="evo-flow">
+                        ${await getEvoTemplate(evo)}
+                    </div>
+                </div>`;
     dialogRef.innerHTML = html;
     showModal();
 }
 
-function getEvoTemplate(evo) {
+async function getEvoTemplate(evo) {
     let chain = evo.chain;
-    let html = "";
-
+    let names = [];
     while (chain) {
-        html += `<p>${chain.species.name}</p>`;
+        names.push(chain.species.name);
         chain = chain.evolves_to[0];
     }
-
+    let promises = [];
+    for (let i = 0; i < names.length; i++) {
+        promises.push(fetchPokemonByName(names[i]));
+    }
+    const pokemons = await Promise.all(promises);
+    let html = "";
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        html += `
+        <div class="evo-item">
+            <img src="${pokemon.sprites.other["official-artwork"].front_default}" height="80">
+            <div>${names[i]}</div>
+        </div>
+        `;
+    }
     return html;
+}
+
+async function fetchPokemonByName(name) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    if (!response.ok) {
+        throw new Error(`HTTP-Error: ${response.status}`);
+    }
+    const pokemon = await response.json();
+    return pokemon;
 }
 
 function getPokeStats(pokemon) {
